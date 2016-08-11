790821f518e124d68bf3ffbc845315e3f230aa15
Amol <amol.bharmoria@gmail.com>
2016-08-11 00:56:34 +0000

    Add date as parameter and print it as Unix Timestamp and Date

--- microservice/urls.py
+++ microservice/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index , name="index"),
+    url(r'^([,\w ]+)/$', views.index , name="index"),
 ]
 
--- microservice/views.py
+++ microservice/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
+import datetime
 from django.template import loader #To load an html template using loader.get_template("name.html")
+from django.utils.dateparse import parse_date 
+from dateutil import parser
+
 
 # Create your views here.
-def index(request):
+def index(request,string):
     template = loader.get_template("index.html")
-    context = {
-            'title':"Timestamp Microservice", # pass context to HTML page i.e passing title here
+    today = parser.parse(string)
+    if(request.GET.get('check')):
+        context ={
+            'title':string,
         }
+        print("ghh")
+    else:
+        context = {
+                'title':string, # pass context to HTML page i.e passing title here
+                'date':today,
+            }
+        print(today)
     return HttpResponse(template.render(context,request)) # Rendering the template
